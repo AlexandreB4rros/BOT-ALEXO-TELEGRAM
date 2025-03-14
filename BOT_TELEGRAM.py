0414e8537b336b663369c297066cad0dc26c1bbc
AlexandreB4rros <147920491+AlexandreB4rros@users.noreply.github.com>
2025-03-14 12:49:16 -0300

    Refactor logging setup to remove file handler and update README with bot selection instructions

--- BOT_TELEGRAM.py
+++ BOT_TELEGRAM.py
@@ -37,20 +37,13 @@ def send_log_to_telegram(message):
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler('LOGGING_EXEC.txt')
-file_handler.setLevel(logging.INFO)
-file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(file_formatter)
-
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 console_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 console_handler.setFormatter(console_formatter)
-
-file_handler.addFilter(IgnoreAttributeErrorFilter())
 console_handler.addFilter(IgnoreAttributeErrorFilter())
 
-logger.addHandler(file_handler)
+
 logger.addHandler(console_handler)
 
 __version__ = "1.0"
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ATENÇÃO - na verção 1.0 usar o comando "pip install ." para instalar a biblioteca especificada da Automação!
+
+Definir qual Bot será usado no telegram:
+    Em DBUG, basta alterar "1" para usar o "ALEXO" e "2" para usar o "TESTE"
+
+    EXEMPLO:
+        DBUG = 1
